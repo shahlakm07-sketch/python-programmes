2d4a2d9825b76933a7038c6155f0b7480cd46431
shahlakm07-sketch <shahlakm07@gmail.com>
2026-05-04 09:19:02 +0300

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,11 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Example
+num = int(input("Enter a number: "))
+print("Prime" if is_prime(num) else "Not Prime")
